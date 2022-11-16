3c7c5e404cb60f9267df3c554ec784a59f3e6bc7
Oldmemory1 <94025104+Oldmemory1@users.noreply.github.com>
2022-11-16 13:59:19 +0800

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct NODE{
-    int w,h;
-    int index;//平面的编号
+    int width,height;
+    int number;//平面的编号
     struct NODE *next;
     struct NODE *previous;
 }node;
@@ -13,50 +13,53 @@ int main(){
     scanf("%d",&N);
     node *head;
     head=(node*)malloc(sizeof(node));
-    head->h=1000001;
+    head->height=1000001;
     node *tail=head,*start=head;
     for(int i=0;i<N;i++){
         node *t;
         t=(node*)malloc(sizeof(node));
         tail->next=t;
-        scanf("%d %d",&t->w,&t->h);
-        t->index=i;
+        scanf("%d %d",&t->width,&t->height);
+        t->number=i;
         t->previous=tail;
-        if(t->h<start->h){start=t;}
+        if(t->height<start->height){start=t;}
         tail=t;
     }
     node *t;
     t=(node*)malloc(sizeof(node));
-    t->h=1000001;
+    t->height=1000001;
     t->previous=tail;
     tail->next=t;
     long long int time=0;
     node *increase=start;//水量增加的平面
-    if(N==1){goto last;}
-    while(1){
-        while(increase->h>increase->previous->h){
+    if(N==1){//goto last;
+	            ans[increase->number]=time+increase->width;}
+    while(N!=1){
+        while(increase->height>increase->previous->height){
             increase=increase->previous;
         }
-        while(increase->h>increase->next->h){
+        while(increase->height>increase->next->height){
             increase=increase->next;
         }
-        node *target=increase;//水量增加的目标平面
-        if(target->previous->h>target->next->h){
+        
+		    node *target=increase;//水量增加的目标平面
+		
+        if(target->previous->height>target->next->height){
             target=target->next;
         }
         else{
             target=target->previous;
         }
-        time+=increase->w;
-        ans[increase->index]=time;
-        time+=increase->w*(target->h-increase->h-1);
+        time+=increase->width;
+        ans[increase->number]=time;
+        time+=increase->width*(target->height-increase->height-1);
         increase->previous->next=increase->next;
         increase->next->previous=increase->previous;
-        target->w+=increase->w;
+        target->width+=increase->width;
         increase=target;
-        if(increase->previous->h==increase->next->h){
-            last:;
-            ans[increase->index]=time+increase->w;
+        if(increase->previous->height==increase->next->height){
+            //last:;
+            ans[increase->number]=time+increase->width;
             break;
         }
     }
